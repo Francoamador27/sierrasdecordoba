979204b2ebcf52eba09787a18171202e8dae94ef
f-amador-eon <101208525+Francoamador27@users.noreply.github.com>
2024-12-14 17:59:44 -0300

    RE CAPTCHA

--- src/componentes/FormLogin.js
+++ src/componentes/FormLogin.js
@@ -3,86 +3,131 @@ import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
 import { addUser } from "../redux/userSlice";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { sanitizeEmail, sanitizePassword } from "./utils.js/sanitize";
 import { url } from "./utils.js/endpoint/endpoint";
 
-export const FormLogin = ()=>{
-    const dispatch = useDispatch()
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "center",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
+export const FormLogin = () => {
+  const dispatch = useDispatch();
+  const [captchaToken, setCaptchaToken] = useState(null); // Estado para almacenar el token del captcha
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "center",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    if (!captchaToken) {
+      return Swal.fire({
+        title: "¡Error!",
+        text: "Por favor, completa el reCAPTCHA.",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    }
+
+    try {
+      const email = await sanitizeEmail(event.target.email.value);
+      const password = await sanitizePassword(event.target.password.value);
+
+      Toast.fire({
+        icon: "info",
+        title: "Iniciando sesión...",
+      });
+
+      const res = await axios.post(
+        `${url}/auth/login`,
+        { email, password, token: captchaToken }, // Incluye el token del captcha
+        { withCredentials: true }
+      );
+
+      if (res.data) {
+        dispatch(addUser(res.data.user));
+        Swal.fire({
+          title: "¡Éxito!",
+          text: "Has iniciado sesión.",
+          icon: "success",
+          confirmButtonText: "Aceptar",
+        });
+      } else {
+        Swal.fire({
+          title: "¡Error!",
+          text: "Usuario o contraseña incorrecta.",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
       }
-    });
-    const handleLogin = async (event) => {
-        event.preventDefault();
-        try {
-          const email =  await sanitizeEmail(event.target.email.value);
-          const password = await sanitizePassword(event.target.password.value);   
-          Toast.fire({
-            icon: "info",
-            title: "logging"
-          });
-          const res = await axios.post(`${url}/auth/login`, { email, password }, { withCredentials: true });
-          if(res.data){
-            dispatch(addUser(res.data.user))
-            Swal.fire({
-              title: '¡Éxito!',
-              text: 'Has iniciado sesion',
-              icon: 'success',
-              confirmButtonText: 'Aceptar',
-            });
-          }else{
-            Swal.fire({
-              title: '¡Error!',
-              text: 'Usuario o contraseña incorrecta.',
-              icon: 'error',
-              confirmButtonText: 'Aceptar',
-            });
-          }
-          
-        } catch (error) {
-          Swal.fire({
-            title: '¡Error!',
-            text: error,
-            icon: 'error',
-            confirmButtonText: 'Aceptar',
-          });
-          console.error('Error al procesar la solicitud:', error);
-        }
-      };
+    } catch (error) {
+      Swal.fire({
+        title: "¡Error!",
+        text: "Hubo un problema al procesar la solicitud.",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+      console.error("Error al procesar la solicitud:", error);
+    }
+  };
 
-
-return(
+  return (
     <>
-        <div className="container-login">
-                <form className="form-log" onSubmit={handleLogin}>
-                <h2>Login</h2>
-                  <div className="input-group ">
-                    <div className="input-group-append">
-                      <span className="input-group-text"><i className="bi bi-envelope-at"></i></span>
-                    </div>
-                    <input type="text" name="email" placeholder="email" className="form-control input_pass"/> 
-                  </div>        
-                  <div className="input-group ">
-                    <div className="input-group-append">
-                      <span className="input-group-text"><i className="bi bi-key"></i></span>
-                    </div>
-                              <input type="text" name="password" placeholder="password" className="form-control input_pass"/> 
-                  </div>
-                 <div className="buttons">
-                  <button type="submit" className="btn btn-primary btn-form">Ingresar</button>
+      <div className="container-login">
+        <form className="form-log" onSubmit={handleLogin}>
+          <h2>Login</h2>
+          <div className="input-group">
+            <div className="input-group-append">
+              <span className="input-group-text">
+                <i className="bi bi-envelope-at"></i>
+              </span>
+            </div>
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              className="form-control input_pass"
+            />
+          </div>
+          <div className="input-group">
+            <div className="input-group-append">
+              <span className="input-group-text">
+                <i className="bi bi-key"></i>
+              </span>
+            </div>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="form-control input_pass"
+            />
+          </div>
 
-                  <button type="button" className="btn btn-primary btn-aux"><Link to={"/register"}>Register </Link></button>
-                 </div>
-                </form>
-                </div>
+          {/* Agregar reCAPTCHA v2 visible */}
+          <ReCAPTCHA
+            sitekey="6LcwvjcqAAAAAKXjL0CHiKhPE_cbMMjI-pXx57yZ" // Reemplaza con tu clave pública
+            onChange={(token) => setCaptchaToken(token)} // Actualiza el token cuando el usuario lo complete
+            onExpired={() => setCaptchaToken(null)} // Maneja el vencimiento del token
+          />
 
-                </>
-)
-}
+          <div className="buttons">
+            <button type="submit" className="btn btn-primary btn-form">
+              Ingresar
+            </button>
+
+            <button type="button" className="btn btn-primary btn-aux">
+              <Link to={"/register"}>Register</Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
